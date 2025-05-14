3e695a6fdeca7e6040f335a9fcfb020d73a0f1ae
Yan Lin <lyan@cs.aau.dk>
2025-05-14 18:25:25 +0200

    Update service list

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 REMOTE_HOST=hetzner
 
 rsync -avP --delete ./ ${REMOTE_HOST}:/root/homelab-dashboard
-ssh ${REMOTE_HOST} "cd /root/homelab-dashboard && docker compose down && docker rmi homelab-dashboard-app && docker compose up -d"
+ssh ${REMOTE_HOST} "cd /root/homelab-dashboard && docker compose down && docker compose up -d --build"
 
 echo "Remote restart completed on ${REMOTE_HOST}"
