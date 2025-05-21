dbf2237a516cce9c7d72385b8c00ebed56253d66
Yan Lin <lyan@cs.aau.dk>
2025-05-21 20:59:22 +0200

    Add services

--- shell.nix
+++ shell.nix
@@ -1,27 +1,28 @@
 { pkgs ? import <nixpkgs> {}, isDev ? true }:
 
 pkgs.mkShell {
-  packages = with pkgs; [
+  packages = with pkgs; if isDev then [
     python312
     python312Packages.pip
     python312Packages.virtualenv
-  ];
+  ] else [];
 
   shellHook = 
     let venvPath = "$HOME/venv/homelab"; 
         preproduction = if isDev then "true" else "false";
         remoteHost = "hetzner";
   in ''
-    export VENV_PATH=${venvPath}
-    export PREPRODUCTION=${preproduction}
+    ${ if isDev then ''
+      export PREPRODUCTION=${preproduction}
 
-    if [ ! -d ${venvPath} ]; then
-      python -m venv ${venvPath}
-    fi
-    source ${venvPath}/bin/activate
-    pip install -r requirements.txt
+      if [ ! -d ${venvPath} ]; then
+        python -m venv ${venvPath}
+      fi
+      source ${venvPath}/bin/activate
+      pip install -r requirements.txt
 
-    ${ if isDev then '' python app.py && exit '' else ''
+      python app.py && exit 
+    '' else ''
       rsync -avP --delete ./ ${remoteHost}:/root/homelab-dashboard --exclude .git
       ssh ${remoteHost} "cd /root/homelab-dashboard && docker compose down && docker compose up -d --build"
       exit
